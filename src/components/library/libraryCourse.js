1d3b6259b453328dee7baac2e74a474d916e1e7b
saydifoth <saydimccall@gmail.com>
2018-07-19 09:46:02 -0600

    built libraryCourse component

---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library-course__title">Problem Solving</label>
+                <div className="library-course__description"> 
+                    <label>Course Description</label>
+                    <p> aldskfj alsdkfj; adsf jlads ;lkjd f;adkfj  askdfl adslfk alsdkfj fkldf jfdk aldkfj </p>    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LibraryCourse;
